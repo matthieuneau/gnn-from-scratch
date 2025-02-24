59d61f4a6c829dfe005db86c8dd83c3db8435a4b
= <=>
2025-02-24 17:07:52 +0100

    compute adj_mat as part of preprocessing instead

--- model.py
+++ model.py
@@ -11,21 +11,11 @@ class VanillaCGN(nn.Module):
         self.n_layers = n_layers
         self.U0 = nn.init.kaiming_normal_(torch.empty(node_dim, input_dim))
         self.b0 = nn.Parameter(torch.randn(node_dim))
-        self.convLayers = [ConvNetLayer(self.node_dim) for _ in range(self.n_layers)]
+        self.convLayers = nn.ModuleList(
+            [ConvNetLayer(self.node_dim) for _ in range(self.n_layers)]
+        )
 
-    @staticmethod
-    def build_adj_mat(edge_index):
-        """Build the adjacency matrix of a graph from edge_index that looks like this: [[1, 4, 5, 6], [2, 3, 3, 5]]
-        cf. https://huggingface.co/datasets/graphs-datasets/ZINC for more details"""
-        n_nodes = max(max(edge_index[0]), max(edge_index[1])) + 1
-        n_edges = len(edge_index[0])
-        adj_mat = torch.zeros((n_nodes, n_nodes))
-        for i in range(n_edges):
-            adj_mat[edge_index[0][i], edge_index[1][i]] = 1
-        return adj_mat
-
-    def forward(self, x, edge_index):
-        adj_mat = self.build_adj_mat(edge_index)
+    def forward(self, x, adj_mat):
         x = x @ self.U0 + self.b0  # self.b0 is broadcasted properly?
         for i in range(self.n_layers):
             x = self.convLayers[i](x, adj_mat)
@@ -48,15 +38,15 @@ class ConvNetLayer(nn.Module):
         return new_x
 
 
-adj_mat = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
-X = torch.tensor([[5, 2], [3, 4], [10, 20]], dtype=torch.float32)
-layer = ConvNetLayer(node_dim=2)
+if __name__ == "__main__":
+    adj_mat = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+    X = torch.tensor([[5, 2], [3, 4], [10, 20]], dtype=torch.float32)
+    layer = ConvNetLayer(node_dim=2)
 
-X = layer(X)
-print(X)
+    X = layer(X)
+    print(X)
 
-
-model = VanillaCGN(input_dim=2, node_dim=2, n_layers=2)
-Y = torch.rand((3, 2))
-Y = model(Y)
-print(Y)
+    model = VanillaCGN(input_dim=2, node_dim=2, n_layers=2)
+    Y = torch.rand((3, 2))
+    Y = model(Y)
+    print(Y)
--- train.py
+++ train.py
@@ -1,7 +1,33 @@
 """Train script designed to work on Zinc dataset. Will make it more modular later"""
 
+import torch
+from datasets import load_dataset
+
 from model import VanillaCGN
 
 input_dim = 1
-node_dim = 20
-model = VanillaCGN(input_dim=input_dim, node_dim=node_dim, n_layers=4)
+node_dim = 10
+model = VanillaCGN(input_dim=input_dim, node_dim=node_dim, n_layers=2)
+
+train_dataset = load_dataset("graphs-datasets/ZINC", split="train[:100]")
+eval_dataset = load_dataset("graphs-datasets/ZINC", split="train[:100]")
+
+
+def build_adj_mat(example):
+    """Build the adjacency matrix of a graph from edge_index that looks like this: [[1, 4, 5, 6], [2, 3, 3, 5]]
+    cf. https://huggingface.co/datasets/graphs-datasets/ZINC for more details"""
+    edge_index = example["edge_index"]
+    n_nodes = max(max(edge_index[0]), max(edge_index[1])) + 1
+    n_edges = len(edge_index[0])
+    adj_mat = torch.zeros((n_nodes, n_nodes))
+    for i in range(n_edges):
+        adj_mat[edge_index[0][i], edge_index[1][i]] = 1
+    adj_mat += torch.eye(adj_mat.shape[0])  # Ensures deg_i > 0 and stabilize training
+    example["adj_mat"] = adj_mat
+    return example
+
+
+train_dataset = train_dataset.map(build_adj_mat)
+
+
+print(train_dataset[0])
