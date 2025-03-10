0664f723cc4cfea667d74bbeeb95dd5633140dfb
= <=>
2025-03-10 16:57:25 +0100

    successful training of gat model

--- GAT.py
+++ GAT.py
@@ -2,14 +2,22 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch
 
+from utils import build_adj_mat
+
+
 class GAT(nn.Module):
-    def __init__(self):
+    def __init__(self, node_dim, hidden_dim, n_classes):
         super(GAT, self).__init__()
-        self.att1 = AttentionLayer(1433, 3000) #1433 is node dim, 3000 is hidden dim
-        self.fc3 = nn.Linear(3000, 7)  # 7 classes for each node, 50 input dim
+        self.att1 = AttentionLayer(
+            node_dim, hidden_dim
+        )  # 1433 is node dim, 3000 is hidden dim
+        self.fc3 = nn.Linear(
+            hidden_dim, n_classes
+        )  # 7 classes for each node, 50 input dim
 
     def forward(self, x, edge_index):
-        x = self.att1(x)
+        adj_mat = build_adj_mat(x, edge_index)
+        x = self.att1(x, adj_mat)
         logits = self.fc3(x)
         return logits
 
@@ -18,15 +26,18 @@ class AttentionLayer(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(AttentionLayer, self).__init__()
         self.W = nn.init.xavier_uniform_(torch.empty(output_dim, input_dim))
-        self.a1 = nn.init.xavier_uniform_(torch.empty(output_dim, 1)))          # a = concat(a1, a2)
-        self.a2 = nn.init.xavier_uniform_(torch.empty(output_dim, 1)))
-        
+        self.a1 = nn.init.xavier_uniform_(
+            torch.empty(output_dim, 1)
+        )  # a = concat(a1, a2)
+        self.a2 = nn.init.xavier_uniform_(torch.empty(output_dim, 1))
+
     def forward(self, x, adj_mat):
-        x = x @ self.W         # matrix containing the W*hi vectors
-        f1 = x @ self.a1
-        f2 = x @ self.a2
-        e = F.leaky_relu_(f1.unsqueeze(1) + f2.unsqueeze(0)) # Broadcast 
-        e = e * adj_mat     # ensures that the softmax is only performed over the neighbors 
-        alpha = F.softmax(e, dim=1) #or dim=0?
-        x = F.elu_(x @ alpha)
+        x = x @ self.W.T  # matrix containing the W*hi vectors
+        f1 = (x @ self.a1).squeeze()
+        f2 = (x @ self.a2).squeeze()
+        e = F.leaky_relu_(f1.unsqueeze(1) + f2.unsqueeze(0))  # Broadcast
+        e = e * adj_mat  # ensures that the softmax is only performed over the neighbors
+        e = torch.where(e == 0, -torch.inf, e)
+        alpha = F.softmax(e, dim=1)  # or dim=0?
+        x = F.elu_(alpha @ x)
         return x
--- train_gat.py
+++ train_gat.py
@@ -1,11 +1,54 @@
-import datasets
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import yaml
 from torch_geometric.datasets import Planetoid
-from utils import build_adj_mat
+import torch.optim as optim
+
+import wandb
+from GAT import GAT
+
+with open("configGAT.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+wandb.init(project="gnn-from-scratch", config=config)
+
+node_dim = config["node_dim"]
+hidden_dim = config["hidden_dim"]
+batch_size = config["batch_size"]
+lr = config["lr"]
+n_classes = config["n_classes"]
+n_epochs = config["n_epochs"]
 
 # 7 classes in the dataset. We calibrate to reproduce the GAT paper
 dataset = Planetoid(
     "./data/", "Cora", num_train_per_class=140, num_val=500, num_test=1000
 )
+data = dataset[0]  # there is only one graph
+# One hot encoding labels for classification task
+data.y = F.one_hot(data.y).float()
 
-print(dataset[0])
-print(build_adj_mat(dataset[0].x, dataset[0].edge_index))
+
+model = GAT(node_dim=node_dim, hidden_dim=hidden_dim, n_classes=n_classes)
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=lr)
+# scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(
+#     optimizer, T_0=3, eta_min=1e-5
+# )
+
+for i in range(n_epochs):
+    optimizer.zero_grad()
+    y_pred = model(data.x, data.edge_index)
+    train_loss = loss_fn(y_pred[data.train_mask], data.y[data.train_mask])
+    train_loss.backward()
+    optimizer.step()
+
+    if i % 10 == 0:
+        with torch.no_grad():
+            y_pred = model(data.x, data.edge_index)
+            valid_loss = loss_fn(y_pred[data.val_mask], data.y[data.val_mask])
+        print(
+            f"Epoch {i:03d} | Train Loss: {train_loss.item():.4f} | Valid Loss: {valid_loss.item():.4f}"
+        )
+    wandb.log({"train_loss": train_loss.item(), "eval_loss": valid_loss.item()})
