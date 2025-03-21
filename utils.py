39cba82fbe42522aaa743478dee7fcd8c4a45f34
VictorJ784 <victor.jesequel@gmail.com>
2025-03-21 23:06:58 +0100

    [ADD] modular code, refact models, test tasks

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 import torch
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
 
 def run_inference(model, n_samples, dataloader):
     with torch.no_grad():
         for i in range(n_samples):
             batch = next(iter(dataloader))
-            y_true = batch["y"]
-            y_pred = model(batch["node_feat"], batch["adj_mat"])
+            y_true = batch.y.to(device)
+            y_pred = model(batch.x.to(device), batch.edge_index.to(device), batch.batch.to(device))
             print("y_true", y_true, "y_pred", y_pred)
 
 
@@ -17,15 +17,3 @@ def set_learning_rate(optimizer, lr):
 
 def get_learning_rate(optimizer):
     return optimizer.param_groups[0]["lr"]
-
-
-def build_adj_mat(node_features, edge_index):
-    """Build the adjacency matrix of a graph from edge_index that looks like this: [[1, 4, 5, 6], [2, 3, 3, 5]]
-    cf. https://huggingface.co/datasets/graphs-datasets/ZINC for more details"""
-    n_nodes = node_features.shape[0]
-    n_edges = len(edge_index[0])
-    adj_mat = torch.zeros((n_nodes, n_nodes))
-    for i in range(n_edges):
-        adj_mat[edge_index[0][i], edge_index[1][i]] = 1
-    adj_mat += torch.eye(adj_mat.shape[0])  # Ensures deg_i > 0 and stabilize training
-    return adj_mat
